8607c73a11e4f0bceaec1aa9dea84585f3bf943e
William Justin <williamjustinxu@gmail.com>
2024-09-29 14:10:48 +0700

    feat: setup material tailwind

--- dynamic-crm/tailwind.config.ts
+++ dynamic-crm/tailwind.config.ts
@@ -1,19 +1,14 @@
 import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 const config: Config = {
     content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
-export default config;
+
+export default withMT(config);
